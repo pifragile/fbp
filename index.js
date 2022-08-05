62397f60f880ce1f8e04f539e92904cf9a074aa2
pifragile <pigu@bluewin.ch>
2022-08-05 11:12:18 +0200

    setup

--- index.js
+++ index.js
@@ -2,27 +2,37 @@ let input;
 let img;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight)
+    createCanvas(windowWidth, windowHeight);
     input = createFileInput(handleFile);
     input.position(0, 0);
+    pixelDensity(1);
 }
 
 function draw() {
     background(255);
-    if (img) {
-        let w, h 
-        if(img.width > img.height) {
-            w = width
-            h = width * img. height / img.width
+    if (img && img.width > 0) {
+        console.log(img);
+        let w, h;
+        if (img.width > img.height) {
+            w = width;
+            h = (width * img.height) / img.width;
         } else {
-            w = height * img.width / img.height
-            h = height
+            w = (height * img.width) / img.height;
+            h = height;
         }
-        image(img, 0, 0, w, h);
-        //noLoop();
+        pg = processImage();
+        image(pg, 0, 0, w, h);
+        noLoop();
     }
 }
 
+function processImage() {
+    pg = createGraphics(img.width, img.height);
+    pg.pixelDensity(1);
+
+    pg.image(img, 0, 0, img.width, img.height);
+    return pg;
+}
 function handleFile(file) {
     print(file);
     if (file.type === "image") {
@@ -33,9 +43,8 @@ function handleFile(file) {
     }
 }
 
-
 function keyPressed() {
     if (keyCode === LEFT_ARROW) {
-        save(`out.png`);
+        pg.save(`out.png`);
     }
 }
