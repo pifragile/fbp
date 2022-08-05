f6a8a6995fc9b56423328b4f8c3daf213fbeff42
pifragile <pigu@bluewin.ch>
2022-08-05 12:00:02 +0200

    random rgb val shift

--- index.js
+++ index.js
@@ -27,10 +27,30 @@ function draw() {
 }
 
 function processImage() {
-    pg = createGraphics(img.width, img.height);
+    let w = img.width;
+    let h = img.height;
+    pg = createGraphics(w, h);
     pg.pixelDensity(1);
 
-    pg.image(img, 0, 0, img.width, img.height);
+    pg.image(img, 0, 0, w, h);
+
+    pg.loadPixels();
+    for (let j = 0; j < h; j++) {
+        for (let i = 0; i < 4 * w; i++) {
+            //if (i % 4 == 3) continue;
+            val = pg.pixels[j * w * 4 + i];
+            f = map(i, 0, 4 * w, 0, 1);
+            let p = random() > f * 0.2
+            if(p) continue
+            newX = Math.round(i + random(-f, f) * 5);
+            newY = Math.round(j + random(-f, f) * 5);
+            if (newX < w * 4 && newY < h) {
+                pg.pixels[newY * w * 4 + newX] = 0;
+            }
+        }
+    }
+    pg.updatePixels();
+
     return pg;
 }
 function handleFile(file) {
