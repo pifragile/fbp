2b2f251b300a63a9e4bd158835e83b4737b7b7b2
pifragile <pigu@bluewin.ch>
2022-08-05 15:19:44 +0200

    nees0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     input = createFileInput(handleFile);
     input.position(0, 0);
-    img = loadImage('AAAA.png')
+    img = loadImage('candy_1.jpg')
     pixelDensity(1);
 }
 
@@ -35,7 +35,9 @@ function processImage() {
 
     pg.image(img, 0, 0, w, h);
 
+
     pg.loadPixels();
+    let s = 0.1
     for (let j = 0; j < h; j++) {
         for (let i = 0; i < w * 4; i += 4) {
             let pix = j * w * 4 + i;
@@ -43,8 +45,9 @@ function processImage() {
             f = map(i, 0, 4 * w, 0, 1);
             let p = random() > f * 2;
             if (p) continue;
-            newX = i + 4 * Math.round(random(-f, f) * 100);
-            newY = Math.round(j + random(-f, f) * 100);
+            f2 = f**2
+            newX = i + 4 * Math.round(random(-f2, f2) * w * s);
+            newY = Math.round(j + random(-f2, f2) * h * s);
             if (newX < w * 4 && newY < h) {
                 for (let k = 0; k < 4; k++) {
                     pg.pixels[newY * w * 4 + newX + k] = pg.pixels[pix + k];
